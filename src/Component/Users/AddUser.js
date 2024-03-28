f6eddb30c6c9382c4f831b9649cd9dca5002580f
sushmabhapkar <sushmabhapkar244@gmail.com>
2024-03-28 10:31:30 +0530

    m3 t4 video 6(resettinf logic and form validation)

--- src/Component/Users/AddUser.js
+++ src/Component/Users/AddUser.js
@@ -5,13 +5,26 @@ import Button from '../UI/Button';
 const AddUser=()=>
 {
 
- const [enteredUsername,setEnteredUsername]=useState('');
+ const [enteredUsername,setEnteredUsername]= useState('');
  const [enteredAge,setEnteredAge]=useState('');
 
       const addUserHandler=(event)=>
       {
            event.preventDefault();
+           //adding validation
+           if(enteredUsername.trim().length===0||enteredAge.trim().length===0)
+           {
+            return;
+           }
+
+           if(+enteredAge<1)
+           {
+            return;
+           }
            console.log(enteredUsername,enteredAge);
+           //form reset logic
+           setEnteredUsername('');
+           setEnteredAge('');
       }
          //function for username
       const usernameChangeHandler=(event)=>
@@ -26,14 +39,25 @@ const AddUser=()=>
           setEnteredAge(event.target.value);
 
       }
+
+
+
  return(
      <Card className={classes.input}>
  <form onSubmit={addUserHandler}>
     <label htmlFor='username'>Name</label>
-    <input id='username' type='text' onChange={usernameChangeHandler}/>
+    <input 
+    id='username' 
+    type='text' 
+    value={enteredUsername}
+    onChange={usernameChangeHandler}/>
 
     <label htmlFor='age'>Age(Year)</label>
-    <input id='age' type='number' onChange={ageChangeHandler}/>
+    <input 
+    id='age' 
+    type='number'
+    value={enteredAge} 
+    onChange={ageChangeHandler}/>
 
     <Button type='submit'>ADD</Button>
    </form>
