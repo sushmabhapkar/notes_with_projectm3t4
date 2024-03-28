7bb62b9fc31898fad0f902b07b971957913026fc
sushmabhapkar <sushmabhapkar244@gmail.com>
2024-03-28 11:34:17 +0530

    m3 t4 video(07 adding userlist )

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import AddUser from './Component/Users/AddUser';
+import UserList from './Component/Users/UserList';
 
 function App() {
   return (
     <div>
          <AddUser/>  
+         <UserList users={[]}/>
     </div>
     
   );
